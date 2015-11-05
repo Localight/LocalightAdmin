597a98e608439df04a61f251a40ec96f8a5828ea
Julian Poyourow <julianpoyo@gmail.com>
2015-11-05 12:52:06 -0800

    Updated services

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -9,8 +9,8 @@
  */
 
 //Get all the giftcards, or create one
-angular.module('angularLocalightApp')
-  .service('Transactions', ['$resource', 'ENV', function($resource, ENV) {
+angular.module('starter', ['ngResource'])
+  .factory('Transactions', ['$resource', function($resource) {
 
     return $resource('http://localhost:3000/transactions', {}, {
       get: {
