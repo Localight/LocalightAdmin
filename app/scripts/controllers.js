61e97d41498eb90a117390097a6496117f94df24
Julian Poyourow <julianpoyo@gmail.com>
2015-11-19 13:54:02 -0800

    Two date pickers and more options

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -52,6 +52,74 @@ angular.module('starter.controllers', [])
       state = false;
       console.log(state);
   }
+
+  $scope.datepickerFrom = {
+     titleLabel: 'Title',  //Optional
+     todayLabel: 'Today',  //Optional
+     closeLabel: 'Close',  //Optional
+     setLabel: 'Set',  //Optional
+     setButtonType : 'button-assertive',  //Optional
+     todayButtonType : 'button-assertive',  //Optional
+     closeButtonType : 'button-assertive',  //Optional
+     inputDate: new Date(),  //Optional
+     mondayFirst: true,  //Optional
+     disabledDates: [], //Optional
+     weekDaysList: [], //Optional
+     monthList: [], //Optional
+     templateType: 'popup', //Optional
+     showTodayButton: 'true', //Optional
+     modalHeaderColor: 'bar-positive', //Optional
+     modalFooterColor: 'bar-positive', //Optional
+     from: new Date(2012, 8, 2), //Optional
+     to: new Date(2018, 8, 25),  //Optional
+     callback: function (val) {  //Mandatory
+       datepickerFromSelect(val);
+     },
+     dateFormat: 'dd-MM-yyyy', //Optional
+     closeOnSelect: false, //Optional
+   };
+
+   var datepickerFromSelect = function (val) {
+      if (typeof(val) === 'undefined') {
+        console.log('No date selected');
+      } else {
+        console.log('Selected date is : ', val)
+      }
+    };
+
+   $scope.datepickerTo = {
+      titleLabel: 'Title',  //Optional
+      todayLabel: 'Today',  //Optional
+      closeLabel: 'Close',  //Optional
+      setLabel: 'Set',  //Optional
+      setButtonType : 'button-assertive',  //Optional
+      todayButtonType : 'button-assertive',  //Optional
+      closeButtonType : 'button-assertive',  //Optional
+      inputDate: new Date(),  //Optional
+      mondayFirst: true,  //Optional
+      disabledDates: [], //Optional
+      weekDaysList: [], //Optional
+      monthList: [], //Optional
+      templateType: 'popup', //Optional
+      showTodayButton: 'true', //Optional
+      modalHeaderColor: 'bar-positive', //Optional
+      modalFooterColor: 'bar-positive', //Optional
+      from: new Date(2012, 8, 2), //Optional
+      to: new Date(2018, 8, 25),  //Optional
+      callback: function (val) {  //Mandatory
+        datepickerToSelect(val);
+      },
+      dateFormat: 'dd-MM-yyyy', //Optional
+      closeOnSelect: false, //Optional
+    };
+
+    var datepickerToSelect = function (val) {
+       if (typeof(val) === 'undefined') {
+         console.log('No date selected');
+       } else {
+         console.log('Selected date is : ', val)
+       }
+     };
 })
 
 .controller('TransactionCtrl', function($scope, $stateParams) {
