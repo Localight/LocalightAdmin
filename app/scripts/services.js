6f57318e562264b1a87e5326c684b3fba43c67b0
Julian Poyourow <julianpoyo@gmail.com>
2015-12-11 14:56:20 -0800

    Added accounts service

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -10,38 +10,73 @@
 
 //Get all the giftcards, or create one
 angular.module('starter.services', ['ngResource'])
+  .factory('Accounts', ['$resource', function($resource) {
+
+    return $resource('http://jnode.ngrok.kondeo.com:8080/admins/:id', {
+      id: '@id'
+    }, {
+      join: {
+        method: 'POST',
+        params: {
+          id: 'join'
+        },
+        isArray: false
+      },
+      login: {
+        method: 'POST',
+        params: {
+          id: 'login'
+        },
+        isArray: false
+      }
+    });
+  }])
   .factory('Transactions', ['$resource', function($resource) {
 
-    return $resource('http://jnode.ngrok.kondeo.com:8080/transactions/:id', { id: '@id' }, {
-        query: {
-            method: 'GET',
-            params: { id: '' },
-            isArray: true
+    return $resource('http://jnode.ngrok.kondeo.com:8080/transactions/:id', {
+      id: '@id'
+    }, {
+      query: {
+        method: 'GET',
+        params: {
+          id: ''
         },
-        get: {
-            method: 'GET',
-            params: { id: '@id' },
-            isArray: false
-        }
+        isArray: true
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id: '@id'
+        },
+        isArray: false
+      }
     });
   }])
   .factory('Payout', ['$resource', function($resource) {
 
-    return $resource('http://jnode.ngrok.kondeo.com:8080/transactions/payouts/:id', { id: '@id' }, {
-        create: {
-            method: 'POST',
-            params: { id: '' },
-            isArray: false
+    return $resource('http://jnode.ngrok.kondeo.com:8080/transactions/payouts/:id', {
+      id: '@id'
+    }, {
+      create: {
+        method: 'POST',
+        params: {
+          id: ''
         },
-        query: {
-            method: 'GET',
-            params: { id: '' },
-            isArray: true
+        isArray: false
+      },
+      query: {
+        method: 'GET',
+        params: {
+          id: ''
         },
-        get: {
-            method: 'GET',
-            params: { id: '@id' },
-            isArray: false
-        }
+        isArray: true
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id: '@id'
+        },
+        isArray: false
+      }
     });
   }]);
